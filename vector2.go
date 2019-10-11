722ab4d39b07bc4d7aec80d23cbd484a29f5424e
EliCDavis <eli.davis1995@gmail.com>
2019-10-10 23:24:07 -0500

    no more pointers

--- vector2.go
+++ vector2.go
@@ -7,59 +7,59 @@ type Vector2 struct {
 	y float64
 }
 
-func NewVector2(x float64, y float64) *Vector2 {
-	return &Vector2{
+func NewVector2(x float64, y float64) Vector2 {
+	return Vector2{
 		x: x,
 		y: y,
 	}
 }
 
-func (v *Vector2) X() float64 {
+func (v Vector2) X() float64 {
 	return v.x
 }
 
-func (v *Vector2) Y() float64 {
+func (v Vector2) Y() float64 {
 	return v.y
 }
 
 // Add returns a vector that is the result of two vectors added together
-func (v Vector2) Add(other *Vector2) *Vector2 {
-	return &Vector2{
+func (v Vector2) Add(other Vector2) Vector2 {
+	return Vector2{
 		x: v.x + other.x,
 		y: v.y + other.y,
 	}
 }
 
-func (v Vector2) Sub(other *Vector2) *Vector2 {
-	return &Vector2{
+func (v Vector2) Sub(other Vector2) Vector2 {
+	return Vector2{
 		x: v.x - other.x,
 		y: v.y - other.y,
 	}
 }
 
-func (v *Vector2) Length() float64 {
+func (v Vector2) Length() float64 {
 	return math.Sqrt((v.x * v.x) + (v.y * v.y))
 }
 
-func (v Vector2) Normalized() *Vector2 {
+func (v Vector2) Normalized() Vector2 {
 	if v.Length() == 0 {
 		return NewVector2(v.x, v.y)
 	}
 	return v.DivByConstant(v.Length())
 }
 
-func (v *Vector2) MultByConstant(t float64) *Vector2 {
-	return &Vector2{
+func (v Vector2) MultByConstant(t float64) Vector2 {
+	return Vector2{
 		x: v.x * t,
 		y: v.y * t,
 	}
 }
 
-func (v *Vector2) DivByConstant(t float64) *Vector2 {
+func (v Vector2) DivByConstant(t float64) Vector2 {
 	return v.MultByConstant(1.0 / t)
 }
 
 // Distance is the euclidian distance between two points
-func (v Vector2) Distance(other *Vector2) float64 {
+func (v Vector2) Distance(other Vector2) float64 {
 	return math.Sqrt(math.Pow(other.x-v.x, 2.0) + math.Pow(other.y-v.y, 2.0))
 }
--- vector3.go
+++ vector3.go
@@ -8,19 +8,19 @@ type Vector3 struct {
 	z float64
 }
 
-func NewVector3(x float64, y float64, z float64) *Vector3 {
-	return &Vector3{
+func NewVector3(x float64, y float64, z float64) Vector3 {
+	return Vector3{
 		x: x,
 		y: y,
 		z: z,
 	}
 }
 
-func Vector3Zero() *Vector3 {
+func Vector3Zero() Vector3 {
 	return NewVector3(0, 0, 0)
 }
 
-func Vector3One() *Vector3 {
+func Vector3One() Vector3 {
 	return NewVector3(1, 1, 1)
 }
 
@@ -36,55 +36,55 @@ func (v Vector3) Z() float64 {
 	return v.z
 }
 
-func (v Vector3) Add(other *Vector3) *Vector3 {
-	return &Vector3{
+func (v Vector3) Add(other Vector3) Vector3 {
+	return Vector3{
 		x: v.x + other.x,
 		y: v.y + other.y,
 		z: v.z + other.z,
 	}
 }
 
-func (v Vector3) Sub(other *Vector3) *Vector3 {
-	return &Vector3{
+func (v Vector3) Sub(other Vector3) Vector3 {
+	return Vector3{
 		x: v.x - other.x,
 		y: v.y - other.y,
 		z: v.z - other.z,
 	}
 }
 
-func (v Vector3) Dot(other *Vector3) float64 {
+func (v Vector3) Dot(other Vector3) float64 {
 	return (v.x * other.x) + (v.y * other.y) + (v.z * other.z)
 }
 
-func (v Vector3) Cross(other *Vector3) *Vector3 {
-	return &Vector3{
+func (v Vector3) Cross(other Vector3) Vector3 {
+	return Vector3{
 		x: (v.y * other.z) - (v.z * other.y),
 		y: -((v.x * other.z) - (v.z * other.x)),
 		z: (v.x * other.y) - (v.y * other.x),
 	}
 }
 
-func (v Vector3) Normalized() *Vector3 {
+func (v Vector3) Normalized() Vector3 {
 	return v.DivByConstant(v.Length())
 }
 
-func (v Vector3) MultByConstant(t float64) *Vector3 {
-	return &Vector3{
+func (v Vector3) MultByConstant(t float64) Vector3 {
+	return Vector3{
 		x: v.x * t,
 		y: v.y * t,
 		z: v.z * t,
 	}
 }
 
-func (v Vector3) MultByVector(o *Vector3) *Vector3 {
-	return &Vector3{
+func (v Vector3) MultByVector(o Vector3) Vector3 {
+	return Vector3{
 		x: v.x * o.x,
 		y: v.y * o.y,
 		z: v.z * o.z,
 	}
 }
 
-func (v Vector3) DivByConstant(t float64) *Vector3 {
+func (v Vector3) DivByConstant(t float64) Vector3 {
 	return v.MultByConstant(1.0 / t)
 }
 
@@ -96,6 +96,6 @@ func (v Vector3) SquaredLength() float64 {
 	return (v.x * v.x) + (v.y * v.y) + (v.z * v.z)
 }
 
-func (v Vector3) Distance(other *Vector3) float64 {
+func (v Vector3) Distance(other Vector3) float64 {
 	return math.Sqrt(math.Pow(other.x-v.x, 2.0) + math.Pow(other.y-v.y, 2.0) + math.Pow(other.z-v.z, 2.0))
 }
