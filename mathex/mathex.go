a3b159516934e967b6bb0cd4bb0e9eb758faae5f
Igor Karatayev <igor.cwer@gmail.com>
2024-04-11 20:34:26 +0300

    Added more mathex functions

--- mathex/mathex.go
+++ mathex/mathex.go
@@ -11,8 +11,32 @@ type Number interface {
 	constraints.Integer | constraints.Float | vector.Number
 }
 
+// Lerp - Calculate linear interpolation between two floats
+func Lerp[T Number](start, end, amount T) T {
+	return start + amount*(end-start)
+}
+
+// Normalize - Normalize input value within input range
+func Normalize[T Number](value, start, end T) T {
+	return (value - start) / (end - start)
+}
+
+// Remap - Remap input value within input range to output range
+func Remap[T Number](value, inputStart, inputEnd, outputStart, outputEnd T) T {
+	return (value-inputStart)/(inputEnd-inputStart)*(outputEnd-outputStart) + outputStart
+}
+
+// Wrap - Wrap input value from min to max
+func Wrap[T Number](value, min, max T) T {
+	return T(float64(value) - float64(max-min)*Floor(float64(value-min)/float64(max-min)))
+}
+
 func Clamp[T Number](f, vmin, vmax T) T {
 	return max(min(f, vmax), vmin)
+}
+
+func Abs[T Number](v T) T {
+	return T(math.Abs(float64(v)))
 }
 
 func Round[T Number](v T) T {
